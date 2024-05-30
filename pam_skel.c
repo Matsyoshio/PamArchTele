f81a7bc8de2c6ae1e2e4111dd1c09e04e26b8a33
Marcio Yoshio Matsushita <marcioymatsushita@outlook.com>
2024-05-30 10:18:06 -0300

    Update pam_skel.c

--- pam_skel.c
+++ pam_skel.c
@@ -18,11 +18,116 @@
  * ----
  */
 
-#include <security/pam_modules.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-/*
- * Authentication realm
- */
+void printTitle(char a[]);
+void stringToCaps(char a[]);
+
+int playRockPaperScissors() {
+    int userHand, computerHand;
+    char userHandString[10], computerHandString[10];
+    
+    int result;
+    int keepAsking;
+    char keepPlaying = 'Y';
+
+    /* Start randomizer */
+    srand(time(NULL));
+
+    while(keepPlaying == 'Y' || keepPlaying == 'y') {
+        /* Generate computer's hand */
+        computerHand = rand() % 3;
+
+        switch(computerHand) {
+            case 0:
+                strcpy(computerHandString, "ROCK");
+                break;
+            case 1:
+                strcpy(computerHandString, "PAPER");
+                break;
+            case 2:
+                strcpy(computerHandString, "SCISSORS");
+                break;
+            default:
+                break;
+        }
+
+        /* Game */
+        printTitle("ROCK, PAPER, SCISSORS BY ZABE");      
+
+        do {
+            printf("\nRock, paper or scissors?: ");
+
+            scanf("%s", userHandString);
+            stringToCaps(userHandString);
+
+            keepAsking = 0;
+
+            if(strcmp(userHandString, "ROCK") == 0)
+                userHand = 0;
+            else if(strcmp(userHandString, "PAPER") == 0)
+                userHand = 1;
+            else if(strcmp(userHandString, "SCISSORS") == 0)
+                userHand = 2;
+            else
+                keepAsking = 1;
+        } while(keepAsking == 1);
+
+        printf("\n\nYour hand: %s", userHandString);
+        printf("\nComputer's hand: %s\n\n", computerHandString);
+
+        result = userHand - computerHand;
+        if(result < 0)
+            result += 3;
+
+        switch(result) {
+            case 0:
+                printf("It's a draw, gg\n\n");
+                return 2; // Empate
+            case 1:
+                printf("YOU WON YAY!\n\n");
+                return 1; // Vitória
+            case 2:
+                printf("Oh, you lost. GG EZ NOOB\n\n");
+                return 0; // Derrota
+            default:
+                break;
+        }
+               
+        printf("Do you want to keep playing? [Y/N]: ");
+        scanf(" %c", &keepPlaying);
+    }
+
+    return 0; // O jogo terminou sem nenhum resultado definido
+}
+
+void printTitle(char a[]) {
+    int j = 0;
+    printf("%c%c", 176, 177);
+    for(int i = 0; i <= strlen(a) + 7; i++)
+        printf("%c", 178);
+    printf("%c%c\n", 177, 176);
+    printf("%c%c%c%c%c ", 176, 177, 178, 177, 176);
+    while(a[j] != '\0') {
+        printf("%c", a[j]);
+        j++;
+    }
+    printf(" %c%c%c%c%c\n", 176, 177, 178, 177, 176);
+    printf("%c%c", 176, 177);
+    for(int i = 0; i <= strlen(a) + 7; i++)
+        printf("%c", 178);
+    printf("%c%c\n", 177, 176);
+}
+
+void stringToCaps(char a[]) {
+    for(int i = 0; i < strlen(a); i++)
+        if(a[i] > 96 && a[i] < 123)
+            a[i] -= 32;
+}
+
 
 int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) 
 {
@@ -35,5 +140,9 @@ int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv )
 
 int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
 {
-	return PAM_SUCCESS;
+	int result = playRockPaperScissors();
+
+    // Retorna PAM_SUCCESS se o jogador venceu ou PAM_AUTH_ERR caso contrário
+    return (result == 1) ? PAM_SUCCESS : PAM_AUTH_ERR;
+
 }
